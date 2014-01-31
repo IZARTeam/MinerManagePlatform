2b4b42d94973977e457987a537b5cd36b27cf3de
IZARTeam <izar.magic@gmail.com>
2014-02-01 01:06:49 +0800

    对返回页面进行压缩

--- DistributeServer/app.js
+++ DistributeServer/app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'jade');
 app.set('view cache', true);
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.compress());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
